42c7905d8b8b52a7fd599a73fc1e8b77cca617a8
Thomas Chatting <tom.chatting@live.co.uk>
2010-06-27 14:16:26 -0700

    Fixed atom feed with validation

--- pixel/atom.php
+++ pixel/atom.php
@@ -3,8 +3,7 @@ include('pxl/pixel.php');
 
 $DateNow = date("d/m/Y");
 $updated = "01/01/1900";
-$articles = articles();
-$articles = array_slice($articles, 0, 10);
+$articles = articles('posts', 10);
 foreach ($articles as $article) {
 	$post = get_post('','','','',$article.'.txt');
 	if ($post[2] <= date("d/m/Y")) {
@@ -17,23 +16,22 @@ foreach ($articles as $article) {
 	$updated = $temp[2]."-".$temp[1]."-".$temp[0];
 }
 ?>
-<?php header("Content-type: text/xml"); ?>
-<?php echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"; ?>
+<?php header("Content-type: text/atom+xml"); ?>
+<?php echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>".PHP_EOL; ?>
 <feed xmlns="http://www.w3.org/2005/Atom">
-<channel>
 	<title><?php echo $blog_title; ?></title>
-	<link href="<?php echo URL; ?>" />
-	<id><?php echo URL; ?></id>
-	<updated><?php echo $updated; ?>T00:00Z</updated>
+	<link href="<?php echo str_replace("atom.php", "", URL); ?>" />
+	<id><?php echo str_replace("atom.php", "", URL); ?></id>
+	<updated><?php echo $updated; ?>T00:00:00Z</updated>
 <?php foreach ($blogPost as $item) { ?>
 	<entry>
 		<title><?php echo htmlentities($item[0]) ?></title>
+		<author><name><?php if (!empty($array[1])) { echo $array[1]; } else { echo author(); } ?></name></author>
 		<?php $temp = explode("/", $item[2]); $date = $temp[2]."-".$temp[1]."-".$temp[0]; ?>
-		<updated><?php echo $date; ?>T00:00Z</updated>
-		<link><?php echo URL.$item[4] ?></link>
+		<updated><?php echo $date; ?>T00:00:00Z</updated>
+		<link href="<?php echo URL.$item[4] ?>" />
 		<id><?php echo URL.$item[4] ?></id>
 		<content type="xhtml"><div xmlns="http://www.w3.org/1999/xhtml"><?php echo $item[3] ?></div></content>
 	</entry>
 <?php } ?>
-</channel>
 </feed>
